652eee88812b7e86effc10dc0afd3d456fea3b99
unknown <kavarony@gmail.com>
2023-04-11 17:30:00 +0530

    about module script error resolved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
@@ -8,9 +8,14 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  constructor(private renderer: Renderer2, private router: Router) {}
 
   ngOnInit() {
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'assets/js/script.js';
+    this.renderer.appendChild(document.head, script);
+
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe(() => {
@@ -18,7 +23,3 @@ export class HomeComponent {
     });
   }
 }
-
-
-
-
